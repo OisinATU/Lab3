8663299c05eeb3d8ea6158d217f023eed40ab946
OisinLoftus-STUDENT <oisinloftus@hotmail.com>
2025-10-06 10:08:25 +0100

    Defined Student Class - name, email, course

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -1,4 +1,37 @@
 package ie.atu;
 
+// Define Class
 public class Student {
+
+    // Define Attributes
+    String name, email, course;
+
+    public Student(String name, String email, String course) {
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getCourse() {
+        return course;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+
+
 }
