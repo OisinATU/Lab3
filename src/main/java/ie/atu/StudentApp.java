afb4262e5a5be3c563c9aeef32b2261dec3e857f
OisinATU <G00470518@atu.ie>
2025-10-08 23:07:01 +0100

    Initialised Arraylist of students

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -1,7 +1,14 @@
 package ie.atu;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StudentApp {
     public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Student> students = new ArrayList<>();
 
 
 
