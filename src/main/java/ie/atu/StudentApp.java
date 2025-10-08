36011e09fe62834c3f2765cd3aac23bf93d16120
OisinATU <G00470518@atu.ie>
2025-10-08 23:18:21 +0100

    Added a while loop to create the number of student objects required

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -11,6 +11,29 @@ public class StudentApp {
         ArrayList<Student> students = new ArrayList<>();
 
 
+        System.out.print("How many students do you want to enter? ");
+        int count = sc.nextInt();
+        sc.nextLine();
+
+        int i = 0;
+        while (i < count) {
+            // TODO: prompt for name, email, course using sc.nextLine()
+            System.out.print("Please enter the name of the student: ");
+            String name = sc.nextLine();
+            System.out.print("Please enter the email of the student: ");
+            String email = sc.nextLine();
+            System.out.print("Please enter the course of the student: ");
+            String course = sc.nextLine();
+
+            // TODO: create a Student using one of your constructors
+            Student student = new Student(name, email, course);
+
+            // TODO: add the new Student to students
+            students.add(student);
+            i++;
+        }
+
+
 
     }
 }
